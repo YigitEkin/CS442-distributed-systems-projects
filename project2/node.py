e74391ca985c00d4c2df56af4adeb26b59c7f362
YigitEkin <yigit.ekin@ug.bilkent.edu.tr>
2022-12-03 17:20:07 +0300

    .

--- project2/node.py
+++ project2/node.py
@@ -1,11 +1,20 @@
 # write a node class that will communicate with the sockets and the server
 # and will be able to send and receive messages
+from multiprocessing import Process
+import random
 import socket
+import sys
 import time
-TOKEN = 1
-REQUEST = 0
-class Node:
-    def __init__(self, dataFile, delta, totalCnt, logFileName, maxTime, sock, rightNode, leftNode, hungry, using, asked, pending_request):
+TOKEN = "1"
+REQUEST = "0"
+
+nodeArray = []
+socketArray = []
+processArray = []
+
+class Node(Process):
+    def __init__(self, dataFile, delta, totalCnt, updateCount, logFileName, maxTime, sock, rightNode, leftNode, hungry, using, asked, pending_request, holder, initialTime):
+        super(Node, self).__init__()
         self.sock = sock
         self.maxTime = maxTime
         self.dataFile = dataFile
@@ -17,85 +26,140 @@ class Node:
         self.hungry = hungry
         self.using = using
         self.asked = asked
+        self.holder = holder
         self.pending_request = pending_request
-        self.logFile = open(logFileName, "w")
-        self.logFile.write("Node started at " + time.strftime("%H:%M:%S") + "\n")
-
-    
-    def start(self):
-
-        # Listen for incoming connections
-        self.sock.listen(1)
-        self.logFile.write("Listening on port " + str(self.sock) + "\n")
-        self.logFile.write("Max time " + str(self.maxTime) + "\n")
-        self.logFile.write("Log file " + str(self.logFileName) + "\n")
-        self.logFile.write("")
-        self.run()
-    
-   
+        self.updateCount = updateCount
+        self.initialTime = initialTime 
+        print("Node started at " + time.strftime("%H:%M:%S") + "\n")
     
     def sendToken(self):
-        self.logFile.write("Sending token to " + str(self.rightNode) + "\n")
+        print("Sending token to " + str(self.rightNode) + "\n")
         msg = 1
-        self.using = False
         if(self.pending_request):
-            tmp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            tmp.connect(('localhost', self.rightNode))
-            tmp.sendto(msg.encode(), ("127.0.0.1", self.rightNode))
+            tmp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            tmp.sendto(msg.encode(), ("localhost", self.rightNode))
             self.pending_request = False
 
 
     def sendRequest(self):
-        self.logFile.write("Sending request to " + str(self.leftNode) + "\n")
+        print("Sending request to " + str(self.leftNode) + "\n")
         msg = 0
-        tmp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        tmp.connect(('localhost', self.leftNode))
-        tmp.sendto(msg.encode(), ("127.0.0.1", self.leftNode))
-       
-    
+        if self.updateCount < self.totalCnt:
+            tmp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            tmp.sendto(msg.encode(), ("localhost", self.leftNode))
+
     def recieve(self):
-        (conn, addr) = self.sock.accept()
-        data = conn.recv(1024).decode()
+        data = self.sock.recv(1024).decode()
         if(data == REQUEST):
-            self.logFile.write("Received token from " + str(self.leftNode) + "\n")
-            self.using = True
-            self.sendToken()
-        elif(data == TOKEN):
-            self.hungry = True
-            if(self.asked==False):
-                self.sendRequest()
-                self.asked = True
-           
-            self.logFile.write("Received request from " + str(self.rightNode) + "\n")
-            if(data == 0 and self.using == False):
+            print("Received token from " + str(self.leftNode) + "\n")
+            if self.holder and not self.using:
                 self.sendToken()
             else:
                 self.pending_request = True
-                if(self.asked == False and data == 1):
+                if self.holder == False and not self.asked:
                     self.sendRequest()
-                    self.asked = True 
-        else:
+                    self.asked = True
+        elif(data == TOKEN):
+            print("Node " + str(self._pid) + " received token")
+            self.holder = True
             self.asked = False
-            if(self.hungry==True):
+            if self.hungry:
                 self.using = True
                 self.hungry = False
             else:
                 self.sendToken()
-                self.pending_request=False
-
-        #conn.close()
-
-        
-    
+                self.pending_request = False
+            
     def close(self):
-        self.logFile.write("Closing socket\n")
+        print("Closing socket\n")
         self.sock.close()
     
     def run(self):
-        counter = 0
-        while True:
-            self.recieve()
-            counter += 1
-            if(counter >= self.totalCnt):
+        server_address = ('localhost', self.port)
+        self.sock.bind(server_address)
+        while self.totalUpdateCount < self.totcount:
+            time.sleep(random.uniform(0, self.maxTime))
+            if self.totalUpdateCount >= self.totcount:
                 break
-        self.close()
+            if self.holder != True:
+                self.hungry = True
+                if not self.asked:
+                    self.sendRequest("request")
+                    self.asked = True
+                if(self.totalUpdateCount < self.totcount):
+                    self.receive()
+            else:
+                self.using = True
+                self.updateDataFile()
+                self.using = False
+                self.hungry = False
+                self.receive()
+                if self.pending_requests:
+                        self.sendToken("token")
+                        self.holder = "other"
+                        self.pending_requests = False
+            print("Node " + str(self.index) + "closing")            
+            self.close()
+
+
+def main():
+    # Check for correct number of command line arguments
+    if len(sys.argv) != 7:
+        print("Usage: python token.py <NP> <DATAFILE> <DELTA> <TOTCOUNT> <LOGFILE> <MAXTIME>")
+        sys.exit(1)
+    # Get the input file name
+    numOfProcess = int(sys.argv[1])
+    if (numOfProcess < 2 or numOfProcess > 20):
+        print("Number of processes must be between 2 and 20")
+        return
+    # Get the input file name
+    inputFileName = sys.argv[2]
+    # Get increment delta
+    delta = int(sys.argv[3])
+    if delta < 0:
+        print("Delta must be greater than 0")
+        return
+    # Get exit count
+    exitCount = int(sys.argv[4])
+    # Get log file name
+    logFileName = sys.argv[5]
+    # Get max time
+    maxTime = int(sys.argv[6])
+    # Initialize the log file
+    logFile = open(logFileName, "w")
+    # Clear the log file
+    logFile.write("")
+    logFile.close()
+    # Initialize the data file
+    dataFile = open(inputFileName, "w")
+    dataFile.write("0\n0")
+    dataFile.close()
+    # Initial time in microseconds
+    initialTime = int(round(time.time() * 1000000))
+
+    for i in range(0, numOfProcess):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        # Bind the socket to the port
+        server_address = ('localhost', 10000 + i)
+        sock.bind(server_address)
+        socketArray.append(sock)
+    # Create processes
+    for i in range(0, numOfProcess):
+        #ataFile, delta, totalCnt, updateCount, logFileName, maxTime, sock, rightNode, leftNode, hungry, using, asked, pending_request
+        if i == 0:
+            p = Process(target=Node, args=(dataFile, delta, 0, logFileName, maxTime, socketArray[i], socketArray[(i+1) % len(socketArray)], socketArray[(i-1) % len(socketArray)], False, False, False, False, True))
+        else:
+            p = Process(target=Node, args=(dataFile, delta, 0, logFileName, maxTime, socketArray[i], socketArray[i+1] % len(socketArray), socketArray[i-1] % len(socketArray), False, False, False, False, False))
+        p.start()
+        processArray.append(p)
+    # Start all processes
+    for i in range(0, len(nodeArray)):
+        processArray[i].start()
+    
+    # Wait for all processes to finish
+    for i in range(0, len(nodeArray)):
+        processArray[i].join()
+
+# Run the main executable
+if __name__ == "__main__":
+    main()
